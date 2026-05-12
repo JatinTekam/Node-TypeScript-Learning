ef212e91b301b6fca8c5f3e887d905a793fdb8ba
JatinTekam <tekamjatin@gmail.com>
2026-05-12 12:25:20 +0530

    add prettierrc.json file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-
-
-function myFun(num:number):number{
-    return num*20;
+function myFun(num: number): number {
+  return num * 20;
 }
 
 console.log(myFun(5));
+
+function myfunTwo() {
+  console.log("Hello world");
+}
+myfunTwo();
