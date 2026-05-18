19345e3c853b7fd4a82a88e73fb2ed04f565c856
JatinTekam <tekamjatin@gmail.com>
2026-05-18 16:19:10 +0530

    add changes in global error handler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -7,6 +7,8 @@ import { config } from "../config/config.js";
 const globalErrorHandler = (error:HttpError,req:Request,res:Response,next:NextFunction)=>{
     const code=error.statusCode || 500;
 
+    
+
     res.status(code).json({
         message: error.message || "Internal Server Error",
         errorStack: config.env === 'development' ? error.stack : '',
