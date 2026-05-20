287f33b1a3f5d458f7cb217611ac513434a37079
JatinTekam <tekamjatin@gmail.com>
2026-05-20 16:03:51 +0530

    add cloudinary

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,10 @@ const _config = {
   dbUrl: process.env.DB_URL,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloudName: process.env.CLOUD_NAME,
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET
+
 };
 
 export const config = Object.freeze(_config);
--- src/controller/bookController.ts
+++ src/controller/bookController.ts
@@ -1,11 +1,62 @@
-import type { NextFunction, Request, Response } from "express";
+import {type NextFunction, type Request, type Response } from "express";
+import cloudinary from "../config/cloudinary.js";
+import createHttpError from "http-errors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const createBook = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.files);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createBook =async (req: Request, res: Response, next: NextFunction) => {
+ try {
+
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  const coverImageFiles = files.coverImage;
+
+  if (!coverImageFiles || coverImageFiles.length === 0) {
+    const erorr=createHttpError(404,"Cover image is required.")
+    return next(erorr);
+  }
+
+  const coverImageMimeType = coverImageFiles[0]?.mimetype.split("/")[1];
+
+   const fileName = coverImageFiles[0]?.filename;
+
+
+   const filePath=path.resolve(__dirname,"../../public/data/uploads",fileName as string);
+
+  const uploadResult = await cloudinary.uploader.upload(filePath,{
+    filename_override: fileName,
+    folder: "book_covers",
+    format: coverImageMimeType
+  });
+
+
+  const bookFile=files.file;
+
+  if(!bookFile || bookFile.length==0){
+    const erorr=createHttpError(404,"Cover image is required.")
+    return next(erorr);
+  }
+
+  const bookFileName=bookFile[0]?.filename;
+
+  const bookFilePath=path.resolve(__dirname,"../../public/data/uploads", bookFileName as string);
+
+
+
+
+  console.log(uploadResult);
 
   res.json({
     message: "Book created successfully",
   });
+ } catch (error) {
+   return next(error);
+ }
+
+ 
 };
 
 export { createBook };
