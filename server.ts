00006ae75dc3e1221e85a065a9a2a89a271bad48
JatinTekam <tekamjatin@gmail.com>
2026-06-02 19:01:40 +0530

    Add Changes In Controller

--- server.ts
+++ server.ts
@@ -3,12 +3,17 @@ import { config } from "./src/config/config.js";
 import dbConnction from "./src/config/db.js";
 
 const startServer = async () => {
-  await dbConnction();
+  try {
+    await dbConnction();
 
   const port = config.port;
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+  } catch (error) {
+    console.log(error.message)
+    process.exit(1);
+  }
 };
 
 startServer();
